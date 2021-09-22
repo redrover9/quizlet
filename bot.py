720c076515abae2be0e2a7bd916f6f5069bc04ad
Grace Thompson <grace.thompson97@gmail.com>
2021-09-22 15:22:27 -0700

    initial commit for bot and json

---
+++ bot.py
@@ -0,0 +1,32 @@
+import discord
+import os
+import json
+import random
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('/question'):
+        with open("quizlet.json") as json_file:
+            data = json.load(json_file)
+            question = random.choice(list(data))
+            print(question)
+            global answer
+            answer = data.get(question)
+            print(answer)
+            await message.channel.send(question)
+    if message.content.startswith('/answer'):
+        with open("quizlet.json") as json_file:
+            data = json.load(json_file)
+            await message.channel.send(answer)
+#client.run(os.getenv('TOKEN'))
+client.run('ODA4MzY5OTIzMjczMzkyMTY5.YCFjKA.nDpHNihhjN8-6H8n2ca0v17A5mU')
